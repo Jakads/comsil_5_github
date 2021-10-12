cdf01905a1a573d369c09f492cea8a34355a5494
Jakads <jakadsjakads@naver.com>
2021-10-12 16:18:24 +0900

    실습 1

---
+++ Stack.h
@@ -0,0 +1,27 @@
+#include "LinkedList.h"
+
+template <class T>
+//1. 템플릿 클래스로 확장해야함
+//2. Stack형식으로 Delete 함수 재정의해야함
+//주의: first, current_size는 class의 멤버 변수이기 때문에 this 포인터를 사용하여 가져와야함
+
+//LinkedList class를 상속받음
+class Stack : public LinkedList<T>{
+	public:
+		virtual bool Delete (T &element){
+			//first가 0이면 false반환
+			if (this->first == 0)
+				return false;
+
+			// LinkedList와 달리 Stack은 current가 가리키는 곳을 삭제
+			Node<T> *current = this->first;
+			Node<T> *next = current->link;
+			
+			element = current->data;
+			delete current;
+			this->first = next;
+			this->current_size--;
+
+			return true;
+			}
+};
